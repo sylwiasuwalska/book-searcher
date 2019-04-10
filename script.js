766f1405f8d2f394e9140a2aca87f67f2fdc19ca
Sylwia Suwalska <sylwia.suwalska@gmail.com>
2019-04-10 23:19:07 +0200

    added new results after scrolling

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 "use strict";
 
-
+const noPreview = "https://www.freeiconspng.com/uploads/no-image-icon-15.png";
 var table = document.getElementById("tableResult");
+var input = document.getElementById("book-name");
+var scrollCounter = 0;
 
-document.getElementById('search').addEventListener('click', searchBooks);
-//document.addEventListener("DOMContentLoaded", searchBooks)
 
-function searchBooks() {
-	var bookName = document.getElementById('book-name').value;
-	var url = 	`https://www.googleapis.com/books/v1/volumes?q=intitle:${bookName}&printType=books&maxResults=4` //filtering by title and type
+document.getElementById('search').addEventListener('click', function(){
+		clearResults();
+    	searchBooks(scrollCounter);
+	}   
+);
+
+input.addEventListener('keyup',function(e){
+	clearResults();
+    if (e.keyCode === 13) {
+    searchBooks(scrollCounter);
+  }  
+});
+
+
+function searchBooks(counter) {
+	var bookName = input.value;
+	var url = 	`https://www.googleapis.com/books/v1/volumes?q=intitle:${bookName}&printType=books&maxResults=5&startIndex=${counter}` //filtering by title and type
 	fetch(url)
 		.then(function(resp) {
 			return resp.json();
@@ -17,46 +31,71 @@ function searchBooks() {
 }
 
 function showBooksList(resp) {
-	table.innerHTML = '';
-	Object.keys(resp.items).forEach(function (ind) {
-		// getting cover of book
-		var image = new Image();
-		if ((resp.items[ind].volumeInfo.imageLinks)===undefined) {
-			image.src = "https://www.freeiconspng.com/uploads/no-image-icon-15.png";
-		} else {
-			var src = resp.items[ind].volumeInfo.imageLinks.thumbnail;
-			image.src = src;
-			if (src===undefined) {
-				image.alt = `No image available.`;
+
+	if (resp.totalItems === 0) {
+		table.innerHTML= `Sorry, we couldn't find what you're looking for!`;
+	} else {
+		
+		Object.keys(resp.items).forEach(function (ind) {
+			// getting cover of book
+			var cover = new Image();
+			if (!resp.items[ind].volumeInfo.imageLinks) {
+				cover.src = noPreview;
+			} else {
+				var src = resp.items[ind].volumeInfo.imageLinks.thumbnail;
+				cover.src = src;
+				if (!src) {
+					cover.src = noPreview;
+					cover.alt = `No image available.`;
+				}
 			}
-		}
-		//getting title of book
-		var title = `${resp.items[ind].volumeInfo.title}`;
-		//getting authors of book
-		var author = `${resp.items[ind].volumeInfo.authors}`;
-		//getting description of book
-		var description = resp.items[ind].volumeInfo.description;
+			//getting title of book
+			var title = `${resp.items[ind].volumeInfo.title}`;
+			//getting authors of book
+			var author = `${resp.items[ind].volumeInfo.authors}`;
+			//getting description of book
+			var description = resp.items[ind].volumeInfo.description;
 
-		// setting the default value in case of lack of description
-		if (description===undefined) {
-			description = `No description available.`
-		}
-		//cutting description to 200 characters
-		if  (description.length > 200) {
-		    description = description.substring(0,199)+"...";
-		}
+			// setting the default value in case of lack of description
+			if (!description) {
+				description = `No description available.`
+			}
+			//cutting description to 200 characters
+			if  (description.length > 200) {
+			    description = description.substring(0,199)+"...";
+			}
 
-		//setting all information for table
-		var summ = `<h4> ${title} </h4> <h5> ${author} </h5> <p> ${description} </p>`
-		var image = `<img src=${image.src}>`
+			//setting all information for table
+			const bookInformationHTML = `<h4> ${title} </h4> <h5> ${author} </h5> <p> ${description} </p>`
+			var bookCover = `<img src=${cover.src}>`
 
-		//creating table
-	    var row = table.insertRow();
-	    var img = row.insertCell(0);
-	    var desc = row.insertCell(1);
+			//creating table
+		    var row = table.insertRow();
+		    var img = row.insertCell(0);
+		    var desc = row.insertCell(1);
 
-	    img.insertAdjacentHTML('beforeend', image);
-	    desc.insertAdjacentHTML('beforeend', summ);
+		    img.insertAdjacentHTML('beforeend', bookCover);
+		    desc.insertAdjacentHTML('beforeend', bookInformationHTML);
 
-	});
+		});
+	};
+}
+
+//adding elements after scrolling all page
+
+window.onscroll = function() {
+
+    var scrollHeight, totalHeight;
+    scrollHeight = document.body.scrollHeight;
+    totalHeight = window.scrollY + window.innerHeight;
+    if(totalHeight >= scrollHeight)
+    {
+        scrollCounter= scrollCounter+5;
+        searchBooks(scrollCounter);
+    }
+}
+
+function clearResults() {
+	table.innerHTML = '';
+	scrollCounter = 0;
 }
