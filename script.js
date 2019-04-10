f3287ae3ef1aa6eea061ea80bd58cb03b808ec21
Sylwia Suwalska <sylwia.suwalska@gmail.com>
2019-04-10 19:57:10 +0200

    working with css

--- script.js
+++ script.js
@@ -4,10 +4,11 @@
 var table = document.getElementById("tableResult");
 
 document.getElementById('search').addEventListener('click', searchBooks);
+//document.addEventListener("DOMContentLoaded", searchBooks)
 
 function searchBooks() {
 	var bookName = document.getElementById('book-name').value;
-	var url = 	`https://www.googleapis.com/books/v1/volumes?q=intitle:${bookName}&printType=books&maxResults=40` //filtering by title and type
+	var url = 	`https://www.googleapis.com/books/v1/volumes?q=intitle:${bookName}&printType=books&maxResults=4` //filtering by title and type
 	fetch(url)
 		.then(function(resp) {
 			return resp.json();
@@ -18,10 +19,10 @@ function searchBooks() {
 function showBooksList(resp) {
 	table.innerHTML = '';
 	Object.keys(resp.items).forEach(function (ind) {
+		// getting cover of book
 		var image = new Image();
-
 		if ((resp.items[ind].volumeInfo.imageLinks)===undefined) {
-			image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShwH8uCYyecy62M0QGsV83eOkaeMLeBoApCVrKyBPNSK03dlSc";
+			image.src = "https://www.freeiconspng.com/uploads/no-image-icon-15.png";
 		} else {
 			var src = resp.items[ind].volumeInfo.imageLinks.thumbnail;
 			image.src = src;
@@ -29,26 +30,27 @@ function showBooksList(resp) {
 				image.alt = `No image available.`;
 			}
 		}
-		console.log(image)
-		console.log(image.src)
-
+		//getting title of book
 		var title = `${resp.items[ind].volumeInfo.title}`;
+		//getting authors of book
 		var author = `${resp.items[ind].volumeInfo.authors}`;
-
+		//getting description of book
 		var description = resp.items[ind].volumeInfo.description;
 
 		// setting the default value in case of lack of description
 		if (description===undefined) {
 			description = `No description available.`
 		}
-		//cutting description to 150 characters
-		if  (description.length > 150) {
-		    description = description.substring(0,149)+"...";
+		//cutting description to 200 characters
+		if  (description.length > 200) {
+		    description = description.substring(0,199)+"...";
 		}
 
+		//setting all information for table
 		var summ = `<h4> ${title} </h4> <h5> ${author} </h5> <p> ${description} </p>`
 		var image = `<img src=${image.src}>`
 
+		//creating table
 	    var row = table.insertRow();
 	    var img = row.insertCell(0);
 	    var desc = row.insertCell(1);
